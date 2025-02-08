76933c490490b3273703a20c92f49d46b1085c97
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-02-08 14:56:25 +0530

    something

--- app/main.py
+++ app/main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
 app = FastAPI()
+
+# Configure CORS to allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,  # Must be False when using allow_origins=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
